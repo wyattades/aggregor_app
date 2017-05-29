0d1d18fbfdfdef7677feec982cd908ef9f3dd2e3
shleewhite <leewhite128@gmail.com>
2017-05-29 14:50:07 -0700

    updated about page

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 
-// TODO
+const styles = StyleSheet.create({ 
+  mainText: {
+    fontSize: 20,
+    marginHorizontal: 30,
+    paddingTop: 20
+  },
+  header:{
+    fontSize: 25,
+    fontWeight: 'bold',
+    padding: 15
+  },
+  subText:{
+  	fontSize: 18,
+  	marginHorizontal: 45,
+  	paddingTop: 5
+  }
+});
 
 const Account = () => (
   <View>
-    <Text>About Page</Text>
+    <Text style={styles.header}>About Aggregor</Text>
+    <Text style={styles.mainText}>Supported News Sources:</Text>
+    <Text style={styles.subText}>• Hacker News</Text>
+    <Text style={styles.subText}>• Reddit</Text>
   </View>
 );
 
