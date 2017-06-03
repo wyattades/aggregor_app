e246434caa4eb5824305b7b983679a0cc7d382f2
shleewhite <leewhite128@gmail.com>
2017-06-02 18:31:28 -0700

    more entry

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -3,8 +3,6 @@ import { View, Text, StyleSheet, TouchableNativeFeedback, Image } from 'react-na
 
 import theme from '../utils/theme';
 
-// TODO: finish designing Entry component
-
 const styles = StyleSheet.create({
   container: {
     height: 256,
@@ -18,12 +16,22 @@ const styles = StyleSheet.create({
     color: theme.TEXT,
   },
   thumbnail: {
-    width: 64,
-    height: 64,
+    width: 80,
+    height: 80,
   },
   author_date: {
     fontSize: 15,
-    color: theme.TEXT_SECOND
+    color: theme.TEXT_SECOND,
+  },
+  author: {
+    position: "absolute",
+    bottom: 16,
+    left: 16
+  },
+  date: {
+    position: "absolute",
+    bottom: 16,
+    right: 16
   }
 });
 
@@ -38,7 +46,18 @@ class Entry extends PureComponent {
     var date = temp.toDateString();
     var time = temp.toLocaleTimeString();
     return (
-        <Text style={styles.author_date}>{date} {time}</Text>
+        <Text style={styles.author_date, styles.date}>{date} {time}</Text>
+      )
+  }
+
+  _title_format(title) {
+    disp_title = title;
+    if (title.length > 150) {
+      disp_title = title.substring(0, 150);
+      disp_title += "..."
+    }
+    return (
+        <Text style={styles.title}>{disp_title}</Text>
       )
   }
 
@@ -48,8 +67,8 @@ class Entry extends PureComponent {
       <TouchableNativeFeedback onPress={onPress}>
         <View style={styles.container}>
           {thumbnailURL ? <Image source={{ uri: thumbnailURL }} style={styles.thumbnail}/> : null}
-          <Text style={styles.title}>{title}</Text>
-          <Text style={styles.author_date}>{author}</Text>
+          {this._title_format(title)}
+          <Text style={styles.author_date, styles.author}>{author}</Text>
           {this._date_format(date)}
         </View>
       </TouchableNativeFeedback>
