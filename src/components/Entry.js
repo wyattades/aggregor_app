1eb042fb35f68d9e79675e57b611b8e81142c9ad
shleewhite <leewhite128@gmail.com>
2017-06-02 18:52:30 -0700

    stuff

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -5,19 +5,34 @@ import theme from '../utils/theme';
 
 const styles = StyleSheet.create({
   container: {
-    height: 256,
+    height: 200,
     marginBottom: 8,
     padding: 16,
     backgroundColor: theme.WHITE,
+  },
+  container_NOIMAGE: {
+    height: 166,
+    marginBottom: 8,
+    padding: 16,
+    backgroundColor: theme.WHITE,
+  },
+  title_NOIMAGE: {
+    fontWeight: '500',
+    fontSize: 18,
+    color: theme.TEXT,
   },
   title: {
     fontWeight: '500',
     fontSize: 18,
     color: theme.TEXT,
+    marginLeft: 90
   },
   thumbnail: {
     width: 80,
     height: 80,
+    position: "absolute",
+    top: 16,
+    left: 16
   },
   author_date: {
     fontSize: 15,
@@ -50,14 +65,14 @@ class Entry extends PureComponent {
       )
   }
 
-  _title_format(title) {
+  _title_format(thumbnailURL, title) {
     disp_title = title;
     if (title.length > 150) {
       disp_title = title.substring(0, 150);
       disp_title += "..."
     }
     return (
-        <Text style={styles.title}>{disp_title}</Text>
+        <Text style={thumbnailURL ? styles.title : styles.title_NOIMAGE}>{disp_title}</Text>
       )
   }
 
@@ -65,9 +80,9 @@ class Entry extends PureComponent {
     const { title, author, date, link, onPress, thumbnailURL } = this.props;
     return (
       <TouchableNativeFeedback onPress={onPress}>
-        <View style={styles.container}>
+        <View style={thumbnailURL ? styles.container : styles.container_NOIMAGE}>
           {thumbnailURL ? <Image source={{ uri: thumbnailURL }} style={styles.thumbnail}/> : null}
-          {this._title_format(title)}
+          {this._title_format(thumbnailURL, title)}
           <Text style={styles.author_date, styles.author}>{author}</Text>
           {this._date_format(date)}
         </View>
